c734d65909e87052e70b26dd2ad38488f7d4c56e
Vincent329 <vincee.ho@gmail.com>
2022-04-21 22:14:50 -0400

    [ADD] - Health Bar event has been set up

---
+++ GAME3033_Final/Assets/_Scripts/UIScripts/HealthUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Slider healthBar;
+    HealthComponent playerHealthComponent;
+
+    private void OnEnable()
+    {
+        PlayerEvents.OnHealthInitialized += OnHealthInitialized;
+    }
+
+    private void OnDisable()
+    {
+        PlayerEvents.OnHealthInitialized -= OnHealthInitialized;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<Slider>();
+    }
+
+    private void OnHealthInitialized(HealthComponent healthComponent)
+    {
+        playerHealthComponent = healthComponent;
+    }
+    public void updateHealthBar()
+    {
+        healthBar.value = playerHealthComponent.CurrentHealth / playerHealthComponent.MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateHealthBar();
+    }
+}
